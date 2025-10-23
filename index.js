e205302fafdfd970aa757848968a379e775e7c23
Biswash Poudel <poudelbiswash27@gmail.com>
2025-10-22 22:08:03 -0400

    changes in index file

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-// Run: npm install ws
+// server.js
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  if (req.url === "/" || req.url === "/index.html") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading index.html");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
+});
+
+// WebSocket server on same port
+const wss = new WebSocket.Server({ server });
 let clients = [];
 
 wss.on("connection", (socket) => {
+  console.log("Client connected");
   clients.push(socket);
-  console.log("New client connected");
 
   socket.on("message", (msg) => {
+    // broadcast to everyone else
     clients.forEach((client) => {
       if (client !== socket && client.readyState === WebSocket.OPEN) {
         client.send(msg.toString());
@@ -16,8 +40,10 @@ wss.on("connection", (socket) => {
   });
 
   socket.on("close", () => {
+    console.log("Client disconnected");
     clients = clients.filter((c) => c !== socket);
   });
 });
 
-console.log("WebSocket signaling server running on ws://localhost:8080");
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
